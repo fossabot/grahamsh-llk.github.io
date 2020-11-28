2ea507b310afb11b7338abaab5cd760315836393
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2020-11-28 09:53:55 -0500

    Update base.js

--- site/assets/js/base.js
+++ site/assets/js/base.js
@@ -1,8 +1,4 @@
-/* =============================================================
-                        INITIAL FUNCTIONS                      
-============================================================= */
 
-// https://stackoverflow.com/a/61511955
 const waitForElement = (selector) => {
   return new Promise((resolve) => {
     if (document.querySelector(selector)) {
@@ -23,38 +19,17 @@ const waitForElement = (selector) => {
   });
 };
 
-/* =============================================================
-                           DARK THEME
-============================================================= */
-
 let darkTheme = false;
-let extensionStyledTheme = false;
 let toggle;
-
-const updateExtensionStyledTheme = () => {
-  document.body.classList.toggle("extension-styled");
-  extensionStyledTheme = !extensionStyledTheme;
-  localStorage.setItem("extensionStyledTheme", extensionStyledTheme);
-};
 
 const updateDarkTheme = (shiftPressed = false) => {
   document.body.classList.toggle("no-animation");
-
-  if (shiftPressed || [...document.body.classList].indexOf(".extension-styled") + 1) {
-    updateExtensionStyledTheme();
-  }
 
   document.body.classList.toggle("dark");
   setTimeout(() => document.body.classList.toggle("no-animation"), 200);
   darkTheme = !darkTheme;
   localStorage.setItem("darkTheme", darkTheme);
 };
-
-if (localStorage.getItem("extensionStyledTheme") === "true") {
-  updateExtensionStyledTheme();
-} else {
-  localStorage.setItem("extensionStyledTheme", false);
-}
 
 if (localStorage.getItem("darkTheme") === "true") {
   updateDarkTheme();
@@ -68,54 +43,25 @@ waitForElement("#dark-toggle").then(() => {
   toggle = document.querySelector("#dark-toggle");
 
   if (darkTheme) {
-    toggle.innerHTML = '<span class="iconify" data-icon="fa-solid:sun" data-inline="false"></span>';
+    $("meta[name=theme-color]").attr("content", "#242424");
+    toggle.innerHTML =
+      '<span class="iconify" data-icon="fa-solid:sun" data-inline="false"></span>';
   } else {
-    toggle.innerHTML = '<span class="iconify" data-icon="fa-solid:moon" data-inline="false"></span>';
+    $("meta[name=theme-color]").attr("content", "#007bff");
+    toggle.innerHTML =
+      '<span class="iconify" data-icon="fa-solid:moon" data-inline="false"></span>';
   }
 
   toggle.addEventListener("click", (event) => {
-    if (event && event.shiftKey) updateExtensionStyledTheme();
     updateDarkTheme();
     if (darkTheme) {
-      toggle.innerHTML = '<span class="iconify" data-icon="fa-solid:sun" data-inline="false"></span>';
+      $("meta[name=theme-color]").attr("content", "#242424");
+      toggle.innerHTML =
+        '<span class="iconify" data-icon="fa-solid:sun" data-inline="false"></span>';
     } else {
-      toggle.innerHTML = '<span class="iconify" data-icon="fa-solid:moon" data-inline="false"></span>';
+      $("meta[name=theme-color]").attr("content", "#007bff");
+      toggle.innerHTML =
+        '<span class="iconify" data-icon="fa-solid:moon" data-inline="false"></span>';
     }
   });
-});
-
-/* =============================================================
-                            TOOLTIPS
-============================================================= */
-
-$(() => {
-  $(document.querySelectorAll('[data-toggle="tooltip"]')).tooltip();
-});
-
-let lastTooltipsAmount = 0;
-
-const tooltipsObserver = new MutationObserver((mutations) => {
-  currentTooltipsAmount = document.querySelectorAll('[data-toggle="tooltip"]').length;
-  if (lastTooltipsAmount !== currentTooltipsAmount) {
-    lastTooltipsAmount = currentTooltipsAmount;
-    $(document.querySelectorAll('[data-toggle="tooltip"]')).tooltip();
-  }
-  // TODO: I suck at mutations. Wanted try to detect added tooltip-ed elements, but it didn't work because I need to traverse the element tree.
-  // console.log(mutations)
-  // mutations.forEach(mutation => {
-  //     console.log(mutation)
-  //     console.log(mutation.addedNodes)
-  //     if (mutation.type === "childList" && mutation.addedNodes.length) {
-  //         console.log(mutation.addedNodes)
-  //         mutation.addedNodes.forEach(node => {
-  //             console.log(node)
-  //             if (node.matches('[data-toggle="tooltip"]')) $(node).tooltip()
-  //         })
-  //     }
-  // })
-});
-
-tooltipsObserver.observe(document.body, {
-  childList: true,
-  subtree: true,
 });
